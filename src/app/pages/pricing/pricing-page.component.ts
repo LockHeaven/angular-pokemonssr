90991985db996c8d8b9a5ec4d3fe72e714e15d9f
Sebastian Contreras <sebastiancontreras02@hotmail.com>
2025-03-31 20:26:37 -0500

    Implement server-side rendering with Angular and Express

--- src/app/pages/pricing/pricing-page.component.ts
+++ src/app/pages/pricing/pricing-page.component.ts
@@ -1,4 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+  PLATFORM_ID,
+} from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'pricing-page',
@@ -6,4 +14,28 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './pricing-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PricingPageComponent {}
+export default class PricingPageComponent implements OnInit {
+  private title = inject(Title);
+  private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
+
+  ngOnInit(): void {
+    // console.log('Hola mundo');
+
+    // console.log(isPlatformBrowser(this.platform));
+
+    // if (isPlatformBrowser(this.platform)) {
+    //   document.title = 'Pricing Page';
+    // }
+
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Este es mi Pricing Page',
+    });
+    this.meta.updateTag({
+      name: 'og:title',
+      content: 'Pricing Page',
+    });
+    this.title.setTitle('Pricing Page');
+  }
+}
