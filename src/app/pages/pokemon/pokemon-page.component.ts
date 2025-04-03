872f48ca1da4c07f1cc41cc26a02447e307f8886
Sebastian Contreras <sebastiancontreras02@hotmail.com>
2025-04-02 22:12:22 -0500

    Add Pokémon interfaces, components, and service for fetching data

---
+++ src/app/pages/pokemon/pokemon-page.component.ts
@@ -0,0 +1,57 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { Pokemon } from '../../pokemons/interfaces';
+import { PokemonService } from '../../pokemons/services/pokemon.service';
+import { ActivatedRoute } from '@angular/router';
+import { tap } from 'rxjs';
+import { Meta, Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'pokemon-page',
+  imports: [],
+  templateUrl: './pokemon-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class PokemonPageComponent implements OnInit {
+  private pokemonsService = inject(PokemonService);
+  private route = inject(ActivatedRoute);
+  private title = inject(Title);
+  private meta = inject(Meta);
+
+  public pokemon = signal<Pokemon | null>(null);
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id') ?? '';
+    this.pokemonsService
+      .loadPokemon(id)
+      .pipe(
+        tap(({ name, id }) => {
+          const pageTitle = `#${id} - ${name}`;
+          const pageDescription = `Página del Pokemon ${name}`;
+          this.title.setTitle(pageTitle);
+
+          this.meta.updateTag({
+            name: 'description',
+            content: pageDescription,
+          });
+          this.meta.updateTag({ name: 'og:title', content: pageTitle });
+          this.meta.updateTag({
+            name: 'og:description',
+            content: pageDescription,
+          });
+          this.meta.updateTag({
+            name: 'og:image',
+            content: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
+          });
+        })
+      )
+      .subscribe((pokemon) => {
+        this.pokemon.set(pokemon);
+      });
+  }
+}
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'navbar',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
